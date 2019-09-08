a67bcf86acf9f8751e5574efafe150b6cadf5f97
Chris Wilding <hello@chriswilding.co.uk>
2019-09-08 21:22:49 +0100

    Add acronym

---
+++ armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -0,0 +1,17 @@
+class ArmstrongNumbers {
+
+	boolean isArmstrongNumber(int numberToCheck) {
+	    var numberOfDigits = Math.floor(Math.log10(numberToCheck)) + 1;
+
+	    System.out.println(numberOfDigits);
+
+	    var total = 0;
+	    for (var i = 0; i < numberOfDigits; i++) {
+            var nthDigit = Double.valueOf(numberToCheck % Math.pow(10, i + 1)).intValue() / Double.valueOf(Math.pow(10, i)).intValue();
+            total += Double.valueOf(Math.pow(nthDigit, numberOfDigits)).intValue();
+        }
+
+	    return numberToCheck == total;
+	}
+
+}
