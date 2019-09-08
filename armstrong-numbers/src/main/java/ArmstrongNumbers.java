6eb64473542212b7de55aef4c8ec6781d77d7600
Chris Wilding <hello@chriswilding.co.uk>
2019-09-08 21:33:25 +0100

    Update armstrong-numbers

--- armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -3,8 +3,6 @@ class ArmstrongNumbers {
 	boolean isArmstrongNumber(int numberToCheck) {
 	    var numberOfDigits = Math.floor(Math.log10(numberToCheck)) + 1;
 
-	    System.out.println(numberOfDigits);
-
 	    var total = 0;
 	    for (var i = 0; i < numberOfDigits; i++) {
             var nthDigit = Double.valueOf(numberToCheck % Math.pow(10, i + 1)).intValue() / Double.valueOf(Math.pow(10, i)).intValue();
