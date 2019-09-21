62c0d2b7ffcfb53c0b525f090bf6bfe80fabf9ec
Chris Wilding <hello@chriswilding.co.uk>
2019-09-21 17:51:48 +0100

    Update bob

--- bob/src/main/java/Bob.java
+++ bob/src/main/java/Bob.java
@@ -1,43 +1,15 @@
-import java.util.stream.Collectors;
-
 class Bob {
-    String hey(String statement) {
-        var input = statement.trim();
+    String hey(String input) {
+        var silence = input.isBlank();
+        if (silence) return "Fine. Be that way!";
 
-        if (isSilence(input)) return "Fine. Be that way!";
-
-        var question = isQuestion(input);
-        var yell = isYell(input);
+        var question = input.trim().endsWith("?");
+        var yell = !input.equals(input.toLowerCase()) && input.equals(input.toUpperCase());
 
         if (question && yell) return "Calm down, I know what I'm doing!";
         if (question) return "Sure.";
         if (yell) return "Whoa, chill out!";
 
         return "Whatever.";
-    }
-
-    private boolean isSilence(String input) {
-        return input.isBlank();
-    }
-
-    private boolean isQuestion(String input) {
-        return input.endsWith("?");
-    }
-
-    private boolean isYell(String input) {
-        var anyAlphabetic = false;
-        var allUpperCase = true;
-
-        for (var c: input.toCharArray()) {
-            if (Character.isAlphabetic(c)) {
-                anyAlphabetic = true;
-
-                if (!Character.isUpperCase(c)) {
-                    allUpperCase = false;
-                }
-            }
-        }
-
-        return anyAlphabetic && allUpperCase;
     }
 }
