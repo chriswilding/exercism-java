d51dc3348f9c92bbae380ee5cec75f88cf02c81b
Chris Wilding <hello@chriswilding.co.uk>
2019-09-21 13:55:40 +0100

    Update darts

--- darts/src/main/java/Darts.java
+++ darts/src/main/java/Darts.java
@@ -1,15 +1,11 @@
 class Darts {
-    private final double x;
-    private final double y;
+    private final double radius;
 
     Darts(double x, double y) {
-        this.x = x;
-        this.y = y;
+        radius = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
     }
 
     int score() {
-        var radius = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-
         if (radius <= 1) {
             return 10;
         } else if (radius <= 5) {
