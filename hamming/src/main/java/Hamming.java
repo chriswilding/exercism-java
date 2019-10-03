5b8b5962109010b7afefa109d6be459de3f815e4
Chris Wilding <hello@chriswilding.co.uk>
2019-10-03 22:26:54 +0100

    Add hamming

---
+++ hamming/src/main/java/Hamming.java
@@ -0,0 +1,37 @@
+class Hamming {
+    private final String leftStrand;
+    private final String rightStrand;
+
+    Hamming(String leftStrand, String rightStrand) {
+        if (leftStrand.isEmpty() && !rightStrand.isEmpty()) {
+            throw new IllegalArgumentException("left strand must not be empty.");
+        }
+
+        if (!leftStrand.isEmpty() && rightStrand.isEmpty()) {
+            throw new IllegalArgumentException("right strand must not be empty.");
+        }
+
+        if (leftStrand.length() != rightStrand.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+
+        this.leftStrand = leftStrand;
+        this.rightStrand = rightStrand;
+    }
+
+    int getHammingDistance() {
+        var hammingDistance = 0;
+
+        for (var i = 0; i < leftStrand.length(); i++) {
+            var leftNucleotide = leftStrand.charAt(i);
+            var rightNucleotide = rightStrand.charAt(i);
+
+            if (leftNucleotide != rightNucleotide) {
+                hammingDistance += 1;
+            }
+        }
+
+        return hammingDistance;
+    }
+
+}
